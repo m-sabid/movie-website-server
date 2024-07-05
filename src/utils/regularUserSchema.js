d68a3c31646591b41d13bb64d781c2694b5615ab
m-sabid <sabidofficial@gmail.com>
2024-07-05 20:18:28 +0600

    remove req on password and cpassword age

--- src/utils/regularUserSchema.js
+++ src/utils/regularUserSchema.js
@@ -4,13 +4,13 @@ const Joi = require('joi');
 const userSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  mobileNumber: Joi.string().required(),
-  age: Joi.string().pattern(/^\d{4}-\d{2}-\d{2}$/).required(),
+  mobileNumber: Joi.string(),
+  age: Joi.string().pattern(/^\d{4}-\d{2}-\d{2}$/),
   profilePicture: Joi.string(),
   gender: Joi.string(),
   role: Joi.string().default('user'),
-  password: Joi.string().required(),
-  confirmPassword: Joi.string().required().valid(Joi.ref('password'))
+  password: Joi.string(),
+  confirmPassword: Joi.string().valid(Joi.ref('password'))
 }).with('password', 'confirmPassword');
 
 // Function to validate user data against the user schema
